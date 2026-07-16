bdd54519344c9116d01346825c98aa0a6902fb18
s-t-e-v <steven.bandaogo@proton.me>
2026-07-16 14:30:02 +0200

    m03/ex01: done

--- module03/ex01/ImageProcessor.py
+++ module03/ex01/ImageProcessor.py
@@ -1,19 +1,23 @@
 import numpy as np
-from matplotlib import image
+from matplotlib import pyplot
+import PIL
 
 class ImageProcessor:
 
     @staticmethod
-    def load(path: str) -> np.ndarray:
+    def load(path: str) -> np.ndarray | None:
         try:
-            img = image.imread(path)
+            img = np.asarray(PIL.Image.open(path))
             print(f"Loading image of dimmensions {img.shape[0]} x {img.shape[1]}")
         except FileNotFoundError:
-            print(f"{path} not found")
+            print(f"Error: {path} not found")
             return None
-        except 
+        except PIL.UnidentifiedImageError:
+            print(f"Error: {path} is not a valid image")
+            return None
         return img
 
     @staticmethod
     def display(array: np.ndarray) -> None:
-        pass
+        pyplot.imshow(array)
+        pyplot.show()
